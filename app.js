871eeb4b5bbc987554ba61a0bc88f1a73a2372e4
bikramgiri <asgbrg.github@gmail.com>
2025-04-08 16:14:52 +0545

    Store filename in database

--- app.js
+++ app.js
@@ -10,11 +10,18 @@ const upload = multer({storage: storage})
 
 const app = express() // Create an instance of express
 
-
 app.set('view engine','ejs') // Set the view engine to ejs
 require("./model/index") // Import the database connection and models from the index.js file in the model folder
 // app.use(express.json()) // Middleware to parse JSON data in request bodies,  frontend and backend are different servers
 app.use(express.urlencoded({extended:true})) // Middleware to parse URL-encoded data (form submissions), frontend and backend are same server
+
+app.get("/",async(req,res)=>{ 
+  const datas = await blogs.findAll()
+    res.render("home", { 
+      blogs : datas 
+    }) 
+}
+)
 
 app.get("/create",(req,res)=>{ // Define a route for the /create URL
     res.render("create") // send a response when the /create route is accessed with the GET method
@@ -24,6 +31,7 @@ app.post("/create",upload.single('image'), async(req,res)=>{ // Define a route f
   // const title = req.body.title // Get the title from the request body
   // const subtitle = req.body.subtitle // Get the subtitle from the request body
   // const description = req.body.description // Get the description from the request body
+  // const image = req.file.filename // Get the filename of the uploaded image from the request file object
   // OR
   const {title,subtitle,description} = req.body // Destructure the title, subtitle, and description from the request body 
   await blogs.create({
