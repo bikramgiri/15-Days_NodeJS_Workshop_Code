8dad7da58c8202f69e0777ae02eec8db80043696
bikramgiri <asgbrg.github@gmail.com>
2025-04-13 06:30:46 +0545

    Fix bugs

--- app.js
+++ app.js
@@ -74,7 +74,8 @@ app.use((err, req, res, next) => {
 sequelize.authenticate()
     .then(() => {
         console.log('Database connection established');
-        app.listen(3000, () => {
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => {
             console.log('Server is running on port 3000');
         });
     })
